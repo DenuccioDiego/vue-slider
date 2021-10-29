018e6a757ec77320739d8b445d33a03885f28e8a
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-29 12:41:18 +0200

    creazione oggetti

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -1,6 +1,37 @@
 var app = new Vue({
      el: '#root',
      data: {
-       message: 'Hello Vue!'
-     }
+          items: [{
+               Image: '01.jpg',
+               title: 'Svezia' ,
+               text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+          },
+
+          {
+
+               Image: '02.jpg',
+               title: 'Svizzera' ,
+               text: 'Lorem ipsum',
+          },
+
+          {
+               Image: '03.jpg',
+               title: 'Gran Bretagna' ,
+               text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+          },      
+     
+          {
+               Image: '04.jpg',
+               title: 'Germania' ,
+               text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+          },
+
+          {
+               Image: '05.jpg',
+               title: 'Paradise' ,
+               text:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+          }
+
+          ]
+        }
    })
